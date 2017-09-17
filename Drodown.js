43519c68c9ac786be03d1df265422b91fedcd1fb
beatm54 <cyberiansph@gmail.com>
2017-09-17 10:00:03 +0800

    Add files via upload

---
+++ Drodown.js
@@ -0,0 +1,2 @@
+
+function myFunction() {document.getElementById("dropok").classList.toggle("show");}window.onclick=function(event) {if(!event.target.matches('.dropbtn')) {var dropdowns=document.getElementsByClassName("drop"); var i; for (i=0; i < dropdowns.length; i++) {var openDropdown=dropdowns[i]; if(openDropdown.classList.contains('show')) {openDropdown.classList.remove('show');}}}}
